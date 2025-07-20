f8138937897fa631990776eaa56f8030f571c945
daniloarauz <daniloarauz@gmail.com>
2025-07-20 12:31:45 -0400

    Update view_machine_data.py

--- view_machine_data.py
+++ view_machine_data.py
@@ -5,6 +5,7 @@ def main():
     hostname = socket.gethostname()
     print(f"Name of the server: {hostname}")
     print(f'The current directory is: {os.getcwd()}')
+    print('Modification for pulling')
 
 if __name__ == "__main__":
     main()
